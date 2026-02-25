8feae731e646a627a54ce8d2b9e105b546c422e4
xThgSilva <camposthiago2021@gmail.com>
2026-02-25 14:27:11 -0300

    init: Inicial structure for backend

---
+++ src/main/java/com/notification/central/NotificationCentralApplication.java
@@ -0,0 +1,13 @@
+package com.notification.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationCentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notification/central/NotificationCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notification.central;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
